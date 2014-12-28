59f0829f3ad866dd908105b047e767c6de62e370
ILYA ERSHOV <ershov.ilya@gmail.com>
2014-12-28 13:50:43 +0300

    Почистил каталог

--- trap4hacker/index.php
+++ trap4hacker/index.php
@@ -45,38 +45,39 @@ if(!$locked) {
     if(DEBUG) echo "Не удалось получить блокировку\n";
     exit(-1);
 }
-else{
+
+if($locked) {
     if(DEBUG) echo "Блокировка на файл получена\n";
-}
+    $output = "-------------------------------------------\n";
+    $output .= "REMOTE_ADDR		" . $_SERVER['REMOTE_ADDR'] . "\n";
+    $output .= "-------------------------------------------\n";
+    $output .= 'Попытка ' . $_SESSION['trap4hacker.attempt_num'] . "		от " . date('j-m-Y H:i:s') . "\n";
+    if ($_SERVER['HTTP_X_REAL_IP'] != $_SERVER['REMOTE_ADDR'] && !empty($_SERVER['HTTP_X_REAL_IP']))
+        $output .= "HTTP_X_REAL_IP	" . $_SERVER['HTTP_X_REAL_IP'] . "\n";
 
-$output="-------------------------------------------\n";
-$output.="REMOTE_ADDR		".$_SERVER['REMOTE_ADDR']."\n";
-$output.="-------------------------------------------\n";
-$output.='Попытка '.$_SESSION['trap4hacker.attempt_num']."		от ".date('j-m-Y H:i:s')."\n";
-if($_SERVER['HTTP_X_REAL_IP'] != $_SERVER['REMOTE_ADDR'])
-$output.="HTTP_X_REAL_IP	".$_SERVER['HTTP_X_REAL_IP']."\n";
+    $domain = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+    if ($domain != $_SERVER['REMOTE_ADDR'])
+        $output .= "HOST DNS NAME		" . $domain . "\n";
 
-$domain	= gethostbyaddr($_SERVER['REMOTE_ADDR']);
-if($domain != $_SERVER['REMOTE_ADDR'])
-$output.="HOST DNS NAME		".$domain."\n";
-
-$output.="HTTP_USER_AGENT	".$_SERVER['HTTP_USER_AGENT']."\n";
-$output.="REQUEST_URI		".$_SERVER['REQUEST_URI']."\n";
-if(!empty($_SERVER['QUERY_STRING']))
-$output.="QUERY_STRING		".$_SERVER['QUERY_STRING']."\n";
+    $output .= "HTTP_USER_AGENT\t\t" . $_SERVER['HTTP_USER_AGENT'] . "\n";
+    $output .= "REQUEST_URI		" . $_SERVER['REQUEST_URI'] . "\n";
+    if (!empty($_SERVER['QUERY_STRING']))
+        $output .= "QUERY_STRING		" . $_SERVER['QUERY_STRING'] . "\n";
 //$output.="REQUEST_TIME		".$_SERVER['REQUEST_TIME']."\n";
 
-if(!empty($info->country->name_ru)) $output.="Страна              ".$info->country->name_ru."\n";
-if(!empty($info->city->name_ru)) $output.="Город	    	    ".$info->city->name_ru."\n";
-if(!empty($info->region->name_ru)) $output.="Регион     	    ".$info->region->name_ru."\n";
+    if (!empty($info->country->name_ru)) $output .= "Страна\t\t\t" . $info->country->name_ru . "\n";
+    if (!empty($info->city->name_ru)) $output .= "Город\t\t\t" . $info->city->name_ru . "\n";
+    if (!empty($info->region->name_ru)) $output .= "Регион\t\t\t" . $info->region->name_ru . "\n";
 
-logWrite($output,$fhBuf);
+    logWrite($output, $fhBuf);
 
 //logWrite(microtime(true) - $startTime, $fhBuf);
 // Закрываем файл
-fflush($fhBuf) or die($php_errormsg);
-flock($fhBuf,LOCK_UN) or die($php_errormsg);
-fclose($fhBuf) or die($php_errormsg);
+    fflush($fhBuf) or die($php_errormsg);
+    flock($fhBuf, LOCK_UN) or die($php_errormsg);
+    fclose($fhBuf) or die($php_errormsg);
+}
+
 unset($fhBuf);
 // Файл освобождён
 
